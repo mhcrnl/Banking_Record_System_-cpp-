6cdc494498aa98eaa4b945f7b76d0c1f908e7791
Rui Kawahara <ruimobile2012@gmail.com>
2017-11-20 00:40:26 -0800

    User can now see menu selection

--- Banking_Record_System_(C++)/RecInfo.cpp
+++ Banking_Record_System_(C++)/RecInfo.cpp
@@ -1,22 +1,46 @@
 #include "RecInfo.h"
+#include <iomanip>
 
 //Default Constructor
 RecInfo::RecInfo()
 {
-	acctNo = 0;
+	dataBase[acctNo] = Customer();
+
+	menu[1] = "Add Record to File";
+	menu[2] = "Show Record from File";
+	menu[3] = "Search Record from File";
+	menu[4] = "Update Record";
+	menu[5] = "Delete Rercord";
+	menu[6] = "Exit";
 }
 
 //Overloaded Constructor
-RecInfo::RecInfo(int account, 
-	const string& first, const string& last, double balance)
+RecInfo::RecInfo(const string& first, const string& last,
+	double balance)
 {
-	dataBase[account] = Customer(first, last, balance);
+	dataBase[acctNo] = Customer(first, last, balance);
+	acctNo++;
+
+	menu[1] = "Add Record to File";
+	menu[2] = "Show Record from File";
+	menu[3] = "Search Record from File";
+	menu[4] = "Update Record";
+	menu[5] = "Delete Rercord";
+	menu[6] = "Exit";
 }
 
 
-void RecInfo::addRecord()
+void RecInfo::showOption()
 {
+	for (auto option : menu)
+		cout << setw(8) << option.first << "-->" << option.second << endl;
+}
 
+void RecInfo::addRecord(const string& first, const string& last, 
+	double balance)
+{
+	dataBase[acctNo] = Customer(first, last, balance);
+	acctNo++;
 }
 
 void RecInfo::printData()
